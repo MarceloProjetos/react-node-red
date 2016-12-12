8ca96107acda027ebf544a54d582d7164d416e77
Marcelo Miranda <pxa255@gmail.com>
2016-12-12 17:24:38 -0200

    directory adjusts

--- start_dev.bat
+++ start_dev.bat
@@ -5,7 +5,7 @@ REM NODE-RED
 start /MIN /D "c:\Program Files\nodejs" cmd /k node-red -v
 
 REM REACT-CREAT-APP
-start /D "c:\Desenvolvimento\git\React\react-bootstrap-exemplo" cmd /k npm start
+start /D "c:\Desenvolvimento\git\React\react-node-red" cmd /k npm start
 
 REM FireFox i
 start /MIN /D "c:\Program Files (x86)\Mozilla Firefox\" cmd /k firefox.exe
