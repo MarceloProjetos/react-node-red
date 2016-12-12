1df577e4b545eb58ec5c1169c4df4a565095d16c
Marcelo Miranda <pxa255@gmail.com>
2016-12-12 16:55:58 -0200

    test2

--- start_dev.bat
+++ start_dev.bat
@@ -7,7 +7,7 @@ start /MIN /D "c:\Program Files\nodejs" cmd /k node-red -v
 REM REACT-CREAT-APP
 start /D "c:\Desenvolvimento\git\React\react-bootstrap-exemplo" cmd /k npm start
 
-REM FireFox
+REM FireFox i
 start /MIN /D "c:\Program Files (x86)\Mozilla Firefox\" cmd /k firefox.exe
 
 
